0ca5b082c77ac174c51042ad8d78efa23e78223e
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-02 01:47:31 +0300

    improve the script

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -1,3 +1,4 @@
+import io
 import logging
 import pandas as pd
 import boto3
@@ -23,52 +24,80 @@ class GlobalVariables:
 
 
 class ModifyColumns:
-    def extract_city_name(self, string):
+    @staticmethod
+    def extract_city_name(string):
         cleaned_string = re.sub(r'[^\w\s]', '', string)
         city_name = cleaned_string.strip()
         return city_name
 
-    def extract_only_numbers(self, string):
+    @staticmethod
+    def extract_only_numbers(string):
         numbers = re.findall(r'\d+', string)
         return ''.join(numbers)
 
-    def extract_floats_without_sign(self, string):
+    @staticmethod
+    def extract_floats_without_sign(string):
         string_without_dollar = string.replace('$', '')
         return float(string_without_dollar)
+
+
+def load_df_from_s3(bucket, prefix, key=None, index_col=None, usecols=None, sep=','):
+    ''' Read a csv from a s3 bucket & load into pandas dataframe'''
+    s3_resource = boto3.resource('s3')
+    try:
+        logging.info(f"Loading {bucket}, {prefix}, {key}")
+        bucket = s3_resource.Bucket(bucket)
+        prefix_objs = bucket.objects.filter(Prefix=prefix)
+        all_data = []
+        for obj in prefix_objs:
+            key = obj.key
+            body = obj.get()['Body'].read()
+            temp_df = pd.read_csv(io.BytesIO(body), encoding='utf8', index_col=index_col, sep=sep)
+            all_data.append(temp_df)
+        return pd.concat(all_data)
+    except Exception as e:
+        logger.exception(f"Error loading data from S3: {e}")
+        raise
+
+
+def data_cleaner(df):
+
+    df['STORE_LOCATION'] = df['STORE_LOCATION'].map(ModifyColumns.extract_city_name)
+    df['PRODUCT_ID'] = df['PRODUCT_ID'].map(ModifyColumns.extract_only_numbers)
+
+    for to_clean in ['MRP', 'CP', 'DISCOUNT', 'SP']:
+        df[to_clean] = df[to_clean].map(ModifyColumns.extract_only_numbers)
+
+    return df
 
 
 def upload_dataframe_into_rds(df):
     table_name = 'clean_transaction'
     sql_query = f"SELECT * FROM {table_name}"
-    database_uri=GlobalVariables.database_uri
+    database_uri = GlobalVariables.database_uri
     try:
         engine = create_engine(database_uri)
         logger.info('Database connection successful')
     except Exception as e:
-        logger.warning('Database connection unsuccessful:', e)
+        logger.error('Database connection unsuccessful:', e)
+        raise
 
     try:
         df.to_sql(table_name, con=engine, if_exists='replace', index=False)
         logger.info(f'Dataframe uploaded into {table_name} successfully')
         uploaded_df = pd.read_sql(sql_query, engine)
-        print(uploaded_df.head())
+        logger.info('\n' + uploaded_df.head().to_string(index=False))
     except Exception as e:
         logger.error('Error happened while uploading dataframe into database:', e)
+        raise
 
 
 def lambda_handler(event, context):
-    now = datetime.now()
-    now_ts = now.strftime("%Y%m%d%H%M%S")
-    s3_client = boto3.client('s3')
-    obj = s3_client.get_object(Bucket="csv-to-s3-project-dogukan-ulu", Key="dirty_store_transactions/dirty_store_transactions.csv")
-    df = pd.read_csv(obj['Body'])
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
 
-    df['STORE_LOCATION'] = df['STORE_LOCATION'].map(lambda x: ModifyColumns.extract_city_name)
+    df = load_df_from_s3(bucket=bucket, prefix=key, sep=',')
 
+    df_final = data_cleaner(df)
 
-    df['PRODUCT_ID'] = df['PRODUCT_ID'].map(lambda x: ModifyColumns.extract_only_numbers)
-
-    for to_clean in ['MRP', 'CP', 'DISCOUNT', 'SP']:
-        df[to_clean] = df[to_clean].map(lambda x: ModifyColumns.extract_only_numbers)
-
-    upload_dataframe_into_rds(df)
+    upload_dataframe_into_rds(df_final)
